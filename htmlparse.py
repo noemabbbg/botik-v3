3902f24ff477c70dc5a7bc9a737162b679809a24
noemabbbg <noemabbbg@gmail.com>
2023-05-12 16:13:57 +0300

    ewrgdrfb

---
+++ htmlparse.py
@@ -0,0 +1,69 @@
+import requests
+from bs4 import BeautifulSoup
+from downloader import P
+from urllib.parse import urlparse
+url = "https://mangalib.me/kaeseul/"
+async def propsParse(url):
+    parsed_url = urlparse(url)
+    # Split the path by '/' and only take the first two elements
+    new_path = '/'.join(parsed_url.path.split('/')[:2])
+    new_url = f'{parsed_url.scheme}://{parsed_url.netloc}{new_path}/'
+    print(new_url)
+
+
+
+    response = requests.get(new_url)
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Get description
+    description_tag = soup.find('meta', {'itemprop': 'description'})
+
+    # Get media tags
+    media_tags = soup.find_all('a', {'class': 'media-tag-item'}, limit=3)
+
+    # Get release year, status, and number of chapters
+    release_year_tag = soup.find('div', text='Год релиза').find_next_sibling()
+    status_tag = soup.find('div', text='Статус тайтла').find_next_sibling()
+    chapters_tag = soup.find('div', text='Загружено глав').find_next_sibling()
+
+    # Get main name
+    main_name_tag = soup.find('div', {'class': 'media-name__main'})
+
+
+
+
+    if main_name_tag:
+        P.u_name = main_name_tag.text
+        
+    # Print results
+    if description_tag:
+        description = description_tag.get('content')
+        P.description = description
+        print("Description:", description)
+
+    if media_tags:
+        print("Media Tags:")
+        for tag in media_tags:
+            k = []
+            k.append(tag.text)
+            
+        P.genres = k
+
+    if release_year_tag:
+        P.release_year = release_year_tag.text
+        print("Release Year:", release_year_tag.text)
+
+    if status_tag:
+        P.manhwa_state = status_tag.text
+        print("Status:", status_tag.text)
+
+    if chapters_tag:
+        P.number_of_chapters = chapters_tag.text
+        print("Chapters:", chapters_tag.text)
+
+
+
+
+
+
---
+++ toPDF.py
@@ -0,0 +1,28 @@
+import os
+from PIL import Image
+
+def images_to_pdf(input_directory):
+    folder_paths = [os.path.join(input_directory, f) for f in os.listdir(input_directory) if os.path.isdir(os.path.join(input_directory, f))]
+
+    for folder_path in folder_paths:
+        images = []
+        for file_name in sorted(os.listdir(folder_path), key=lambda x: int(x.split('.')[0])):
+            if file_name.endswith('.jpg'):
+                image_path = os.path.join(folder_path, file_name)
+                images.append(Image.open(image_path).convert('RGB'))
+
+        if not images:
+            print(f"Папка {folder_path} не содержит изображений в формате .jpg")
+            continue
+
+        pdf_path = os.path.join(input_directory, f"{os.path.basename(folder_path)}.pdf")
+        images[0].save(pdf_path, save_all=True, append_images=images[1:], format='PDF', quality=100)
+
+        for image in images:
+            image.close()
+        import shutil
+        shutil.rmtree(folder_path)
+       
+        
+
+
